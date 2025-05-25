30d81e87e1489fc1ceb3d96423983c61aea7810d
muazbinkhalid <maazkhalid889@gmail.com>
2025-05-25 00:18:54 -0700

    Create dynamicmain.cpp

---
+++ Week 2/dynamicmain.cpp
@@ -0,0 +1,25 @@
+//EXAMPLE 2.2:
+//#include <iostream>
+//#include "stack1.h"
+//
+//using namespace std;
+//
+//int main() {
+//    stack<int> intstack;
+//    stack<float> flstack;
+//
+//    int data;
+//    float value;
+//
+//    intstack.push(35);
+//    flstack.push(3.14);
+//
+//    intstack.pop(data);
+//    cout << data << endl;
+//
+//    flstack.pop(value);
+//    cout << value << endl;
+//
+//    return 0;
+//}
+//
