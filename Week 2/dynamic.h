5acf4ecac6f039dda196701461e5542422dc87f8
muazbinkhalid <maazkhalid889@gmail.com>
2025-05-25 00:17:44 -0700

    Create dynamic.h

---
+++ Week 2/dynamic.h
@@ -0,0 +1,20 @@
+// EXAMPLE 2.2:
+//#pragma once
+//
+//template<typename itemtype>
+//class stack {
+//private:
+//    int top;
+//    int maxstack;
+//    itemtype* items;
+//public:
+//    stack();
+//    stack(int max);
+//    ~stack();
+//
+//    bool Isempty();
+//    bool Isfull();
+//
+//    void push(itemtype newitem);
+//    void pop(itemtype& newitem);
+//};
