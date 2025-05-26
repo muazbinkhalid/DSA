ad1d9b458eada802f64fc38ba82d537139de18f4
muazbinkhalid <maazkhalid889@gmail.com>
2025-05-26 08:11:06 -0700

    Create queue.h

---
+++ Week 5/queue.h
@@ -0,0 +1,35 @@
+//EXERCISE 5.1:
+//#pragma once
+//#include <iostream>
+//template <typename T>
+//class queue {
+//private:
+//    int front, rear, maxqueue, count;
+//    T* items;
+//
+//public:
+//    queue(int m = 100); 
+//    ~queue(); 
+//    bool isempty() const;
+//    bool isfull() const;
+//    void insert(T newitem);
+//    void remove(T& item);
+//};
+
+//EXERCISE 5.2:
+//#pragma once
+//#include <iostream>
+//template <typename T>
+//class queue {
+//private:
+//    int front, rear, maxqueue, count;
+//    T* items;
+//
+//public:
+//    queue(int m = 100); 
+//    ~queue(); 
+//    bool isempty() const;
+//    bool isfull() const;
+//    void insert(T newitem);
+//    void remove(T& item);
+//};
